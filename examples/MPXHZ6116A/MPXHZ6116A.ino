60314867ad0431a8e13afe89cf11d75990946a93
James Young <17939249+jamesy0ung@users.noreply.github.com>
2024-07-22 08:43:52 +1000

    Update MPXHZ6116A.ino

--- examples/MPXHZ6116A/MPXHZ6116A.ino
+++ examples/MPXHZ6116A/MPXHZ6116A.ino
@@ -1,29 +1,19 @@
 #include <MPXHZ6116A.h>
 
-#define SENSOR_PIN A0
-#define SUPPLY_VOLTAGE 5.0
+const int analogPin = A0; 
 
-MPXHZ6116A pressureSensor(SENSOR_PIN, SUPPLY_VOLTAGE);
+MPXHZ6116A pressureSensor(analogPin); 
 
 void setup() {
   Serial.begin(9600);
-  Serial.println("MPXHZ6116A Pressure Sensor Example");
 }
 
 void loop() {
-  float pressure = pressureSensor.readPressure();
-  Serial.print("Pressure: ");
-  Serial.print(pressure, 2);
-  Serial.print(" kPa (");
-  Serial.print(pressure * 10, 2);
-  Serial.println(" hPa)");
+  float compensatedPressure = pressureSensor.readPressureWithTempComp(25.0); 
 
-  float compensatedPressure = pressureSensor.readPressureWithTempComp(25.0);
   Serial.print("Compensated Pressure: ");
-  Serial.print(compensatedPressure, 2);
-  Serial.print(" kPa (");
-  Serial.print(compensatedPressure * 10, 2);
-  Serial.println(" hPa)");
+  Serial.print(compensatedPressure);
+  Serial.println(" kPa");
 
-  delay(1000);
+  delay(2000);
 }
